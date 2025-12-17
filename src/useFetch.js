2a7d837d40c85c05af7b73b17c9ed167d057103b
dpakks <somasundaram.d@northeastern.edu>
2025-12-17 02:37:44 -0500

    POST and DELETE Implemented

--- src/useFetch.js
+++ src/useFetch.js
@@ -2,16 +2,15 @@
 import { useState, useEffect } from "react";
 
 const useFetch = (url) => {
+    console.log("URL:", url)
     const [data, SetData] = useState(null);
     const [isPending, SetIsPending] = useState(true);
     const [error, setError] = useState(null)
 
     useEffect(() => {
         const abortCont = new AbortController();
-
-
         setTimeout(() => {
-            fetch(url, {signal: abortCont.signal})
+            fetch(url, { signal: abortCont.signal })
                 .then(res => {
                     console.log("Response", res)
                     if (!res.ok) {
@@ -26,14 +25,14 @@ const useFetch = (url) => {
                     setError(null)
                 })
                 .catch((e) => {
-                    console.log("Error Message:", e)
+                    if (e.name === 'AbortError') {
+                        console.log("Fetch Aborted")
+                    }
                     SetIsPending(false);
                     setError(e.message)
                 })
         }, 0);
-
         return () => abortCont.abort();
-
     }, [url]);
 
     return { data, isPending, error }
